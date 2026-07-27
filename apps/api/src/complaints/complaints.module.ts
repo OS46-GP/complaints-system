9453ff2496112d4e97422d57a6006ae6e3b635be
Mahmoud Ismail <mahmoudelnfiky@gmail.com>
2026-07-27 15:29:40 +0300

    feat(api): register complaints module

--- apps/api/src/complaints/complaints.module.ts
+++ apps/api/src/complaints/complaints.module.ts
@@ -1,4 +1,10 @@
 import { Module } from "@nestjs/common";
+import { ComplaintsController } from "./complaints.controller";
+import { ComplaintsService } from "./complaints.service";
+import { ComplaintFilesService } from "./complaint-files.service";
 
-@Module({})
+@Module({
+  controllers: [ComplaintsController],
+  providers: [ComplaintsService, ComplaintFilesService],
+})
 export class ComplaintsModule {}
