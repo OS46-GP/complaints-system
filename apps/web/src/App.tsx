86c8f9f3b283fd258df001b25af87084d550c39f
mohamed-fouad-nassar <mohammednassar740@gmail.com>
2026-08-01 01:13:25 +0300

    refactor(toast): replace react-hot-toast library with sonner from schadcn library to match our styles

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -5,26 +5,24 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import AppRouter from "./router";
 import { queryClient } from "./lib/query-client";
 
-import { ThemeProvider, useTheme } from "./components/shared/theme-provider";
 import { TooltipProvider } from "./components/ui/tooltip";
+import { ThemeProvider, useTheme } from "./components/shared/theme-provider";
 
-function AppContent() {
+function ThemedToaster() {
   const { theme } = useTheme();
 
-  return (
-    <QueryClientProvider client={queryClient}>
-      <AppRouter />
-      <Toaster position="top-right" theme={theme} duration={4000} />
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
-  );
+  return <Toaster position="top-right" theme={theme} />;
 }
 
 export default function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <TooltipProvider delayDuration={0}>
-        <AppContent />
+        <QueryClientProvider client={queryClient}>
+          <AppRouter />
+          <ThemedToaster />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
       </TooltipProvider>
     </ThemeProvider>
   );
